a239c5536e789b77a5c4cdec54eaa73667568c27
Philippe <pfaist@ethz.ch>
2015-04-23 11:28:43 +0200

    Improved MATLAB analysis tool

--- tools/analyze_tomorun_histogram.m
+++ tools/analyze_tomorun_histogram.m
@@ -1,9 +1,20 @@
-function analyze_tomorun_histogram(histfname, varargin)
+function dat = analyze_tomorun_histogram(histfname, varargin)
 
   optdefs = struct;
   optdefs.FitThresFrac = struct('arg', 'n', 'default', 1e-3);
   optdefs.IgnoreStartPoints = struct('arg', 'n', 'default', 0);
-  
+  optdefs.FigHandleP = struct('arg', 'any', 'default', [], ...
+                              'validator', @(x) get_figure_handle(x, 'intgr-fid-epsilon'));
+  optdefs.FigHandleLogP = struct('arg', 'any', 'default', [], ...
+                                 'validator', @(x) get_figure_handle(x, 'fit-intgr-fid-epsilon'));
+  optdefs.ClearFigures = struct('switch', true);
+  optdefs.CustomFit = struct('switch', false);
+  optdefs.FitModel = struct('arg', 'fh*', 'default', [], ...
+                            'imply', {{'CustomFit'}});
+  optdefs.FitWhich = struct('arg', 's*', 'default', [], ...
+                            'validator', validator_enum('LogP', 'P'), ...
+                            'imply', {{'CustomFit'}});
+
   [opts, ~] = parse_opts(optdefs, varargin);
   
   FIT_THRES_FRAC = opts.FitThresFrac;
@@ -21,51 +32,150 @@ function analyze_tomorun_histogram(histfname, varargin)
   P = AvgCounts/trapz(-OneMinusFidelity,AvgCounts);
   ErrorP = Error/trapz(-OneMinusFidelity,AvgCounts);
   
-  mynamedfigure('intgr-fid-epsilon');
-  clf;
-  errorbar(OneMinusFidelity,P,ErrorP, 'Marker', '.', 'LineStyle', 'none');
+  % get the "support" of P -- where P != 0
+  P_IndNonZero = find(P>0);
+  % and the width of one bin (NOTE: ASSSUMPTION THAT BINS EQUALLY SPACED)
+  %bin_width = abs(Fidelity(2)-Fidelity(1))
+  
+  figure(opts.FigHandleP);
+  if (opts.ClearFigures)
+    clf;
+  end
+  errorbar(OneMinusFidelity, P, ErrorP, ...
+           'Marker', '.', 'LineStyle', 'none');
   set(gca, 'YScale', 'linear')
 
   % calculate logarithm of P
   LogP = log(P);
   ErrorLogP = ErrorP ./ P; % delta(log x) = delta(x) / x
 
-  % do the fit
-  [MaxP, MaxPInd] = max([0;P(2:end-1)]); % note: prepend zero so indexes match in P
+  % --- fit model ---
+  if (opts.CustomFit)
+    assert(~isempty(opts.FitModel), 'No Fit Model provided (''FitModel'' option).');
+    assert(~isempty(opts.FitWhich), ['You need to specify which data to fit to (''FitWhich''= ' ...
+                        '''P''/''LogP'')']);
+    thefitfunc = opts.FitModel;
+    if (strcmp(opts.FitWhich, 'LogP'))
+      fitlogp = true;
+    else
+      fitlogp = false;
+    end
+    fitoptions = opts.FitOptions;
+  else
+    fitlogp = true;
+    thefitfunc = @(a, m, c, x) -a*x + m*log(x) + c;
+    display(thefitfunc);
+    fitoptions = {'StartPoint', [1, 1, 100]};
+  end    
+  % --- prepare data for the fit ---
+  [MaxP, MaxPInd] = max(P);
   FitDataIdx = P>FIT_THRES_FRAC*MaxP;
-  LastFitDataIdx = find(FitDataIdx,1);
+  FitDataIdx = FitDataIdx(1:end-IGNORE_START_POINTS);
+  % --- prepare fit data x, y and weights ---
   FitDataX = OneMinusFidelity(FitDataIdx);
-  FitDataY = LogP(FitDataIdx);
-  FitDataWeights = 1 ./ ErrorLogP(FitDataIdx);
-  % possibly restrict which points are used
-  FitDataX = FitDataX(1:end-IGNORE_START_POINTS);
-  FitDataY = FitDataY(1:end-IGNORE_START_POINTS);
-  FitDataWeights = FitDataWeights(1:end-IGNORE_START_POINTS);
-
-  % --- do fit ---
-  thefitfunc = @(a, b, c, x) -a*x + b*log(x) + c
+  if (fitlogp)
+    FitDataY = LogP(FitDataIdx);
+    FitDataWeights = 1 ./ ErrorLogP(FitDataIdx);
+  else
+    FitDataY = P(FitDataIdx);
+    FitDataWeights = 1 ./ ErrorP(FitDataIdx);
+  end
+  % --- now, do the fit ---
   [thefit, gof] = fit(FitDataX, FitDataY, thefitfunc, ...
                       'Weights', FitDataWeights, ...
-                      'StartPoint', [1, 1, 100] ...
+                      fitoptions{:} ...
                       );
+
+  if (fitlogp)
+    evalfitlogp = @(x) thefit(x);
+    evalfitp = @(x) exp(thefit(x));
+  else
+    evalfitlogp = @(x) log(thefit(x));
+    evalfitp = @(x) thefit(x);
+  end
+
   % ---
-                    
+  %thefitfunc = @(a, b, m, c, x) -a*x -b*x.^2 + m*log(x) + c
+  %evalfitlogp = @(p, x) thefitfunc(p.a, p.b, p.m, p.c, x);
+  %[thefit, gof] = fit(FitDataX, FitDataY, thefitfunc, ...
+  %                    'Weights', FitDataWeights, ...
+  %                    'StartPoint', [1, 0, 1, 100] ...
+  %                    );
+  % ---
+  %thefitfunc = @(a, c, x) -a*x + log(x) + c
+  %evalfitlogp = @(p, x) thefitfunc(p.a, p.c, x);
+  %[thefit, gof] = fit(FitDataX, FitDataY, thefitfunc, ...
+  %                    'Weights', FitDataWeights, ...
+  %                    'StartPoint', [1, 100] ...
+  %                    );
+  % ---
+  %thefitfunc = @(a, c, x) c*x.*exp(-a*x)
+  %evalfitlogp = @(p, x) log(thefitfunc(p.a, p.c, x));
+  %[thefit, gof] = fit(FitDataX, P(FitDataIdx), thefitfunc, ...
+  %                    'Weights', 1 ./ ErrorP(FitDataIdx), ...
+  %                    'StartPoint', [1, 100] ...
+  %                    );
+  % ---
+  
   display(thefit);
 
-  mynamedfigure('fit-intgr-fid-epsilon');
-  clf;
+  figure(opts.FigHandleLogP);
+  if (opts.ClearFigures)
+    clf;
+  end
   hold on;
   %plot(thefit, OneMinusFidelity, log(P));
-  errorbar(OneMinusFidelity, P, ErrorP, 'Marker', '.', 'LineStyle', 'none', 'Color', [0.6, 0.6, 1.0]);
+  errorbar(OneMinusFidelity(P_IndNonZero), P(P_IndNonZero), ErrorP(P_IndNonZero), ...
+           'Marker', '.', 'LineStyle', 'none', 'Color', [0.6, 0.6, 1.0]);
   plot(FitDataX, exp(FitDataY), 'rx');
-  xx = linspace(min(FitDataX), max(FitDataX),200);
-  plot(xx, exp(thefitfunc(thefit.a, thefit.b, thefit.c, xx)), 'r-');
+  xxmin = min(OneMinusFidelity(P_IndNonZero));
+  xxmax = max(OneMinusFidelity(P_IndNonZero));
+  xx = linspace(xxmin, xxmax, 200);
+  plot(xx, evalfitp(xx), 'r-');
   set(gca, 'YScale', 'log');
   
   % ---
   % plot the fit back onto the first figure
-  mynamedfigure('intgr-fid-epsilon');
+  figure(opts.FigHandleP);
   hold on;
-  plot(xx, exp(thefitfunc(thefit.a, thefit.b, thefit.c, xx)), 'r-');
+  plot(xx, evalfitp(xx), 'r-');
+  
+  % ---
+  % Prepare return data
+  % ---
+  if (nargout > 0)
+    dat = struct;
+    dat.Fidelity = Fidelity;
+    dat.OneMinusFidelity = OneMinusFidelity;
+    dat.AvgCounts = AvgCounts;
+    dat.ErrorAvgCounts = Error;
+    dat.P = P;
+    dat.LogP = LogP;
+    dat.ErrorP = ErrorP;
+    dat.ErrorLogP = ErrorLogP;
+    dat.P_IndNonZero = P_IndNonZero;
+    dat.fitlogp = fitlogp;
+    dat.FitDataX = FitDataX;
+    dat.FitDataY = FitDataY;
+    dat.FitDataWeights = FitDataWeights;
+    dat.thefitfunc = thefitfunc; % in whatever fit space
+    dat.evalfitlogp = evalfitlogp; % always log(P)
+    dat.evalfitp = evalfitp; % always P
+    dat.thefit = thefit;
+  end
 
+end
+
+
+function figh = get_figure_handle(val, defaultnamedfigure)
+  if (isempty(val))
+    figh = mynamedfigure(defaultnamedfigure);
+  elseif (isnumeric(val))
+    figh = val;
+  elseif (ischarstring(val))
+    figh = mynamedfigure(val);
+  else
+    error('analyze_tomorun_histogram:badFigureHandle', 'Bad figure handle: %s', ...
+          dispstr(val));
+  end
 end
