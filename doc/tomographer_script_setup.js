f81c510aecf01ff7103f5550f0a0c1048ae6c1c2
Philippe <pfaist@ethz.ch>
2015-09-03 12:52:31 +0200

    doc style update

--- doc/tomographer_script_setup.js
+++ doc/tomographer_script_setup.js
@@ -21,10 +21,49 @@ function setupAllDirectoryLinks(tag) {
 	      });
 };
 
+function setupSpanArrows(tag) {
+    // see http://stackoverflow.com/a/16292287/1694896
+    var fixspanarrow = function(obj) {
+	var content = obj.text().trim();
+	obj.toggleClass('arrowright', content.indexOf('\u25BA') != -1);
+	obj.toggleClass('arrowdown', content.indexOf('\u25BC') != -1);
+    };
+    $("table.directory", tag)
+    .each(function() {
+	      $("span.arrow", tag)
+	      .each(function(){
+			fixspanarrow($(this));
+			$(this).bind("DOMSubtreeModified", function() {
+					 fixspanarrow($(this));
+				     });
+		    });
+	  });
+};
+
+function fixNavTreeArrows(tag) {
+    var fixarrows = function (tree) {
+	$("img[src='arrowdown.png']", tree).attr('src', 'myarrowdown.png');
+	$("img[src='arrowright.png']", tree).attr('src', 'myarrowright.png');
+    };
+    (function(fixeddivobject){
+	 var observer = new MutationObserver(function(mutations) {
+						 fixarrows(fixeddivobject);
+					     });
+	 observer.observe(fixeddivobject, {
+			      childList: true,
+			      attributes: true,
+			      attributeFilter: ['src'],
+			      subtree: true
+			  });
+     })($("div#nav-tree", tag)[0]);
+};
+
 
 $(document).ready(function() {
 		      setupExternalLinks($(document));
 		      setupAllDirectoryLinks($(document));
+		      setupSpanArrows($(document));
+		      fixNavTreeArrows($(document));
 		 });
 
 
