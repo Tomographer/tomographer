a74ae3464a8b176827155d1597a5bbf2ded4b9d2
Philippe <pfaist@ethz.ch>
2015-09-24 00:41:14 +0200

    updated repo URL

--- mk_dist.py
+++ mk_dist.py
@@ -58,7 +58,7 @@ if os.path.exists(tomo_name_w_ver) or any((os.path.exists(n) for n in tomo_name_
 
 print("Packaging Tomographer version", gitversion, "as", tomo_name_w_ver)
 
-tomographer_url = "git@gitlab.phys.ethz.ch:pfaist/tomographer.git"
+tomographer_url = "git@github.com:Tomographer/tomographer.git"
 
 class MyStore: pass
 e = MyStore()
