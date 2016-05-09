95e3cf0eb256ac521088bd0f4a643269b27ec0a4
Philippe <pfaist@ethz.ch>
2016-05-09 10:43:06 +0200

    added comment

--- tools/reskew_logmu_curve.m
+++ tools/reskew_logmu_curve.m
@@ -1,6 +1,8 @@
 function [a2 a1 c] = reskew_logmu_curve(a, x0, y0, m)
 % reskew_logmu_curve -- apply skew to parabola via m*log(x) term
 %
+% Implements the inverse function of deskew_logmu_curve.
+%
 
   a2 = a - m ./ (2*x0.^2);
   a1 = m ./ x0 - 2*a2.*x0;
