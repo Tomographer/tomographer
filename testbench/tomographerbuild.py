49b818eb0ac30b023477c40e15eaf0fa4bdac81d
Philippe Faist <phfaist@gmail.com>
2017-05-14 20:59:43 -0700

    added cheap benchmarks

---
+++ testbench/tomographerbuild.py
@@ -0,0 +1,115 @@
+
+from __future__ import print_function
+
+import os
+import os.path
+import json
+import datetime
+import hashlib
+import multiprocessing # cpu_count for make -jX
+import subprocess
+import shutil
+import timeit
+
+
+TOMOGRAPHER_URL = os.environ.get('TOMOGRAPHER_URL',
+                                 'file://'+os.path.expanduser("~/Research/projects/tomographer"))
+
+GIT = os.environ.get('GIT', "git")
+CMAKE = os.environ.get('CMAKE', "cmake")
+MAKE = os.environ.get('MAKE', "make")
+
+base_cmake_args = []
+
+benchwork_dir = '_benchwork'
+
+def do_run(cmdargv, **kwargs):
+
+    add_environ = kwargs.pop('add_environ', {})
+    if add_environ:
+        kwargs['env'] = dict(os.environ, **add_environ)
+
+    print("Running  %r  ... kwargs=%r"%(cmdargv,kwargs))
+    subprocess.check_call(cmdargv, **kwargs)
+    print("")
+
+class TomographerBuild(object):
+    def __init__(self, tomographer_version, cmake_args, add_environ={}):
+
+        self.tomographer_version = tomographer_version
+        self.cmake_args = cmake_args
+        self.add_environ = add_environ
+
+        m = hashlib.sha256()
+        m.update(self.tomographer_version)
+        m.update("\n".join(self.cmake_args))
+        m.update("\n".join((str(k)+"\n"+str(v) for k,v in self.add_environ.items())))
+        self.key = m.hexdigest()[-16:]
+
+        self.workdir = os.path.join(benchwork_dir, self.key)
+        self.tomographer_version_dir = 'tomographer-'+self.tomographer_version
+        self.work_tomographer_dir = os.path.join(self.workdir, self.tomographer_version_dir)
+        self.work_tomographer_build = os.path.join(self.work_tomographer_dir, 'build')
+        
+        try:
+            self._compile()
+        except Exception:
+            print("Error compiling Tomographer ", self.tomographer_version)
+            shutil.rmtree(self.workdir)
+            raise
+
+    def _compile(self):
+        if not os.path.isdir(self.workdir):
+
+            print("NEW BUILD: Tomographer %s, key=%s"%(self.tomographer_version, self.key))
+
+            os.mkdir(self.workdir)
+            # dump info in there so we know which version it is
+            with open(os.path.join(self.workdir, 'BUILD_INFO'), 'w') as f:
+                json.dump({'key': self.key,
+                           'tomographer_version': self.tomographer_version,
+                           'cmake_args': self.cmake_args,
+                           'build_datetime': datetime.datetime.now().isoformat()}, f,
+                          indent=4)
+
+            # git clone
+            do_run([GIT, "clone", TOMOGRAPHER_URL, self.tomographer_version_dir],
+                   cwd=self.workdir, add_environ=self.add_environ)
+            # git checkout <CORRECT-VERSION>
+            do_run([GIT, "checkout", self.tomographer_version],
+                   cwd=self.work_tomographer_dir, add_environ=self.add_environ)
+
+            os.mkdir(self.work_tomographer_build)
+            # cmake
+            do_run([CMAKE, '..', ] + self.cmake_args,
+                   cwd=self.work_tomographer_build, add_environ=self.add_environ)
+
+            # make
+            do_run([MAKE, '-j'+str(multiprocessing.cpu_count()), 'VERBOSE=1'],
+                   cwd=self.work_tomographer_build, add_environ=self.add_environ)
+
+
+
+
+
+
+def timeit_call(args, cwd, repeat=3, number=1):
+    timeit_call.args = args
+    timeit_call.cwd = cwd
+
+    xx = timeit.repeat('subprocess.check_call(args, cwd=cwd)',
+                      setup="""
+import subprocess
+from tomographerbuild import timeit_call
+args = timeit_call.args
+cwd = timeit_call.cwd
+""", repeat=repeat, number=number)
+
+    print("\nTimeIt Report:  repeat=%d, number=%d\n"
+          "\targs=%r\n"
+          "\tcwd=%r"%(repeat, number, args, cwd))
+    for x in xx:
+        print("Time: %.3g s"%(x/number))
+    print("Best time: ", min([ x/number for x in xx]), "s")
+    print("\n")
+
---
+++ testbench/tomorun.py
@@ -0,0 +1,83 @@
+
+from __future__ import print_function
+
+import os
+import os.path
+import subprocess
+
+from tomographerbuild import TomographerBuild, timeit_call
+
+
+cmake_release_opts = [
+    '-DCMAKE_BUILD_TYPE=Release',
+    '-DTARGET_ARCHITECTURE=auto',
+    '-DCMAKE_CXX_FLAGS_RELEASE=-O3 -g -UNDEBUG',
+]
+
+build_v1_0_optim_release = TomographerBuild('v1.0', cmake_release_opts + [
+    '-DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang',
+    '-DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++',
+    # for openmp:
+    '-DCMAKE_EXE_LINKER_FLAGS=-L/usr/lib -L/usr/local/opt/llvm/lib',
+    ], add_environ={'LDFLAGS': '-L/usr/lib -L/usr/local/opt/llvm/lib'})
+
+build_v4_1_optim_release = TomographerBuild('v4.1', cmake_release_opts + [
+    '-DCMAKE_CXX_COMPILER=/usr/bin/clang',
+    '-DCMAKE_CXX_COMPILER=/usr/bin/clang++',
+    ])
+
+build_g287a5ae_optim_release = TomographerBuild('v5.0aaa-15-g287a5ae', cmake_release_opts + [
+    '-DCMAKE_CXX_COMPILER=/usr/bin/clang',
+    '-DCMAKE_CXX_COMPILER=/usr/bin/clang++',
+    ])
+
+build_g287a5ae_GCC6_optim_release = TomographerBuild('v5.0aaa-15-g287a5ae', cmake_release_opts + [
+    '-DCMAKE_C_COMPILER=/usr/local/opt/gcc/bin/gcc-6',
+    '-DCMAKE_CXX_COMPILER=/usr/local/opt/gcc/bin/g++-6',
+    '-DBoost_INCLUDE_DIR=/opt/boost-1_64_0-gcc6/include',
+    '-DBoost_PROGRAM_OPTIONS_LIBRARY_RELEASE=/opt/boost-1_64_0-gcc6/lib/libboost_program_options.a',
+    '-DTOMORUN_MULTIPROC=openmp',
+    ])
+
+
+# timeit_call(
+#     [
+#         os.path.abspath(os.path.join(build_v1_0_optim_release.work_tomographer_build,
+#                                      'cxx', 'tomorun', 'tomorun')),
+#         '--config',
+#         'tomorun-config-EntglWitness'
+#     ],
+#     cwd='examples/two-qubits-Bell')
+
+
+# timeit_call(
+#     [
+#         os.path.abspath(os.path.join(build_v4_1_optim_release.work_tomographer_build,
+#                                      'tomorun', 'tomorun')),
+#         '--config',
+#         'tomorun-config-EntglWitness'
+#     ],
+#     cwd='examples/two-qubits-Bell')
+
+
+# timeit_call(
+#     [
+#         os.path.abspath(os.path.join(build_g287a5ae_optim_release.work_tomographer_build,
+#                                      'tomorun', 'tomorun')),
+#         '--config',
+#         'tomorun-config-EntglWitness',
+#         '--no-control-step-size',
+#         '--no-control-binning-converged',
+#     ],
+#     cwd='examples/two-qubits-Bell')
+
+timeit_call(
+    [
+        os.path.abspath(os.path.join(build_g287a5ae_GCC6_optim_release.work_tomographer_build,
+                                     'tomorun', 'tomorun')),
+        '--config',
+        'tomorun-config-EntglWitness',
+        '--no-control-step-size',
+        '--no-control-binning-converged',
+    ],
+    cwd='examples/two-qubits-Bell')
