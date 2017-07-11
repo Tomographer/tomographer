d307282381bd4dcfc8c4db00ed1de115044dead6
Philippe Faist <phfaist@gmail.com>
2017-07-10 19:57:01 -0700

    better analysis plots

--- testbench/full_tomorun_bench_analysis.py
+++ testbench/full_tomorun_bench_analysis.py
@@ -1,6 +1,9 @@
 
 import re
 import os.path
+import itertools
+
+import numpy as np # np.prod
 
 
 class Store(object):
@@ -29,6 +32,22 @@ def load_results(res_file_name):
 
     return Store(settings_list=settings_list, results=results, settings_matrix=settings_matrix)
 
+def merge_results(*dlist):
+
+    dmerged = Store(settings_list=[], results=[], settings_matrix={})
+
+    for d in dlist:
+        dmerged.settings_list += d.settings_list
+        dmerged.results += d.results
+        for k,v in d.settings_matrix.items():
+            if k not in dmerged.settings_matrix:
+                dmerged.settings_matrix[k] = {}
+            if isinstance(v, list):
+                dmerged.settings_matrix[k].update(dict([(vl,vl) for vl in v]))
+            else:
+                dmerged.settings_matrix[k].update(v)
+
+    return dmerged
 
 
 default_lbl_formatters = {
@@ -36,7 +55,7 @@ default_lbl_formatters = {
     'tomorun_static': str,
     'asserts': lambda v: '+a' if v else '-a',
     'multiproc': lambda v: {'openmp':'OMP','cxxthreads':'C++','sequential':'seq'}.get(v, v),
-    'compiler': lambda v: os.path.basename(v[0]),
+    'compiler': lambda v: os.path.basename(v),
     'optim': lambda v: v,
     'target_arch': lambda v: 'arch:{}'.format(v),
 }
@@ -46,6 +65,14 @@ class _ValFormatterWithKey(object):
     def __call__(self, value):
         return "{}={}".format(self.key, value)
 
+class _MkLabel(object):
+    def __init__(self, lbl_dict, fld_order):
+        self.lbl_dict = lbl_dict
+        self.fld_order = fld_order
+
+    def __call__(self, sett, valaccess=lambda x: x.valname):
+        return " ".join([ self.lbl_dict.get(f,_ValFormatterWithKey(f))(valaccess(sett[f]))
+                          for f in self.fld_order ])
 
 def subset_results(filt_dict, d, fld_order, lbl_dict={}):
     idx = [i for i, x in enumerate(d.settings_list)
@@ -58,26 +85,32 @@ def subset_results(filt_dict, d, fld_order, lbl_dict={}):
     lbl_dict_ok = dict(default_lbl_formatters)
     lbl_dict_ok.update(lbl_dict)
 
-    def mklabel(i):
-        #return " ".join([ "{}={}".format(k,d.settings_list[i][k].valname) for k in d.settings_list[i]])
-        return " ".join([ lbl_dict_ok.get(f,_ValFormatterWithKey(f))(d.settings_list[i][f].valname) for f in fld_order if f not in filt_dict ])
+    #def _mklabel(i):
+    #    #return " ".join([ "{}={}".format(k,d.settings_list[i][k].valname) for k in d.settings_list[i]])
+    #    return " ".join([ lbl_dict_ok.get(f,_ValFormatterWithKey(f))(d.settings_list[i][f].valname) for f in fld_order if f not in filt_dict ])
 
-    return Store(d=d, didx=idx, n=len(idx),
+    mklabel = _MkLabel(lbl_dict_ok, [ f for f in fld_order if f not in filt_dict ])
+
+    return Store(d=d, filt_dict=filt_dict, didx=idx, n=len(idx),
                  fld_order=fld_order,
                  sett=[d.settings_list[i] for i in idx],
                  resbest=[d.results[i]['best'] for i in idx],
                  res=[d.results[i] for i in idx],
-                 settinglabels=[ mklabel(i) for i in idx ]
+                 settinglabels=[ mklabel(d.settings_list[i]) for i in idx ],
+                 lbl_dict=lbl_dict_ok,
                  )
 
 
-def simple_subset_plot(subd, spacings=[1,1]):
+def simple_subset_plot(subd, spacings=[1,1],
+                       colorsettings=['tomorun_static','asserts'],
+                       colorfn='jet'):
 
+    import matplotlib.patches as mpatches
     import matplotlib.pyplot as plt
     
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    fig.subplots_adjust(bottom=0.3)
+    fig.subplots_adjust(left=0.04, right=0.96, bottom=0.3)
 
     # pad with zeros up to length of fld_order
     spacings = spacings + [0]*(len(subd.fld_order) - len(spacings))
@@ -96,9 +129,100 @@ def simple_subset_plot(subd, spacings=[1,1]):
         xpos.append(curpos)
         curpos += 1
 
-    ax.bar(xpos, subd.resbest, align='center', color=[0.8,0.2,0], alpha=0.8)
+    class colorfn_cmap(object):
+        def __init__(self, cmap):
+            import matplotlib.colors
+            import matplotlib.cm
+            num_colors = np.prod([len(subd.d.settings_matrix[colorsetting])
+                                  for colorsetting in colorsettings])
+            cNorm = matplotlib.colors.Normalize(vmin=0, vmax=num_colors-1)
+            scalarMap = matplotlib.cm.ScalarMappable(norm=cNorm, cmap=cmap)
+
+            #print([k for k in subd.d.settings_matrix['tomorun_static']])
+
+            self.color_settings_list = list(itertools.product(
+                *[ [ (sfld, svl) for svl in subd.d.settings_matrix[sfld] ]
+                  for sfld in colorsettings ]
+                ))
+            #print("color_settings_list=", self.color_settings_list)
+
+            assert num_colors == len(self.color_settings_list)
+
+            self.settings_to_colors = [
+                (dict(self.color_settings_list[k]), scalarMap.to_rgba(k),)
+                for k in range(num_colors)
+                ]
+        
+            #print('settings_to_colors=', self.settings_to_colors)
+
+        def __call__(self, d):
+            #print("d=", d)
+            return [ c for valset, c in self.settings_to_colors
+                     if valset == d ][0]
+
+    if not callable(colorfn):
+        colorfn = colorfn_cmap(colorfn)
+
+    mklabelx = _MkLabel(subd.lbl_dict, [f for f in subd.fld_order if f not in subd.filt_dict and f not in colorsettings])
+    mklabelcolor = _MkLabel(subd.lbl_dict, colorsettings)
+
+    barcolors = [ ]
+    legends_for_sett = {}
+    for j in range(len(subd.sett)):
+        
+        if subd.resbest[j] is None:
+            continue
+    
+        settdic = dict([ (k,v.valname)
+                           for k,v in subd.sett[j].items() if k in colorsettings ])
+        thiscolor = colorfn(settdic)
+        barcolors.append( thiscolor )
+
+        settdicfs = frozenset(settdic.items())
+        if settdicfs not in legends_for_sett:
+            legends_for_sett[settdicfs] = mpatches.Patch(color=thiscolor, label=mklabelcolor(settdic, valaccess=lambda x: x))
+            
+
+    #print('barcolors=',barcolors)
+
+    settinglabels = [ mklabelx( dict([(k,v) for k,v in subd.sett[j].items()]) )
+                      for j in range(len(subd.sett)) ]
+
+    #print("Legend..ary:\n", list(zip(* legends_for_sett.items() )), sep='')
+
+    ax.legend(handles=list(legends_for_sett.values()))
+
+    ax.bar(xpos, subd.resbest, align='center', color=barcolors, alpha=0.8)
     ax.set_xticks(xpos)
-    ax.set_xticklabels(subd.settinglabels, rotation=90)
+    ax.set_xticklabels(settinglabels, rotation=90)
+    for item in ax.get_xticklabels():
+        item.set_fontsize(8)
+
+    ax.set_xlim(-sum(spacings), max(xpos)+sum(spacings))
 
     plt.show()
     
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser(os.path.basename(__file__))
+    parser.add_argument('bench_files', nargs='+')
+
+    args = parser.parse_args()
+
+    ds = []
+    for fn in args.bench_files:
+        ds.append(load_results(fn))
+
+    d = merge_results(*ds)
+
+    subd = subset_results(
+        {'optim':'O3', 'target_arch':'auto',},
+        d,
+        ['version','compiler','tomorun_static','asserts','multiproc']
+    )
+
+    simple_subset_plot(subd, spacings=[0,1,1])
