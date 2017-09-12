ee4553c0fa2b6e52ab83b1be41ccfcba1101ba6d
Philippe Faist <phfaist@gmail.com>
2017-09-11 18:11:02 -0700

    travis tweak

--- test/travis/travis-install.sh
+++ test/travis/travis-install.sh
@@ -1,13 +1,15 @@
 
 set -x
 
+
+
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
 
-    source travis-install-linux.sh
+    source test/travis/travis-install-linux.sh
 
 elif [[ $TRAVIS_OS_NAME == "osx" ]]; then
 
-    source travis-install-osx.sh
+    source test/travis/travis-install-osx.sh
 
 fi
 
