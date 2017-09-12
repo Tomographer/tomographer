a7a79b950591769da0305e6b9aee679b819b93a8
Philippe Faist <phfaist@gmail.com>
2017-09-11 20:56:09 -0700

    travis tweak; added osx & conda env

--- test/travis/travis-install-osx.sh
+++ test/travis/travis-install-osx.sh
@@ -4,6 +4,8 @@
 #
 
 
+INSTALL_BOOST_USING_CONDA_GCC=0
+
 #
 # C/C++ compiler
 #
@@ -12,6 +14,13 @@ if [[ "$TT_CC" == "clang-osx" ]]; then
     export CMAKE_C_COMPILER="/usr/bin/clang"
     export CMAKE_CXX_COMPILER="/usr/bin/clang++"
     
+elif [[ "$TT_CC" == "gcc-conda" ]]; then
+    
+    # use gcc/g++ compiler provided by conda
+    export CMAKE_C_COMPILER="$HOME/.miniconda/bin/gcc"
+    export CMAKE_CXX_COMPILER="$HOME/.miniconda/bin/g++"
+    INSTALL_BOOST_USING_CONDA_GCC=1
+
 else
     
     echo &>2 "TOMOGRAPHER TRAVIS TEST SETUP ERROR: Unknown TT_CC=$TT_CC"
@@ -31,6 +40,14 @@ if [[ "$TT_PYTHON" == "brew-python-3" ]]; then
     export CMAKE_ADD_ARGS="$CMAKE_ADD_ARGS -DPYTHON_LIBRARY=/usr/local/opt/python3/Frameworks/Python.framework/Versions/3.6/lib/libpython3.6m.dylib"
     export PIP=/usr/local/bin/pip3
     export INSTALL_PYTHON_DEPS_USING="brew-pip"
+
+elif [[ "$TT_PYTHON" == "conda-python-2.7" ]]; then
+
+    export MINICONDA_INSTALLER=https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
+    export PYTHON_EXECUTABLE=$HOME/.miniconda/bin/python
+    export CMAKE_ADD_ARGS="$CMAKE_ADD_ARGS -DCMAKE_PREFIX_PATH=$HOME/.miniconda/envs/test-environment -DPYTHON_LIBRARY=$HOME/.miniconda/lib/libpython2.7.so -DPYTHON_INCLUDE_DIR=$HOME/.miniconda/include/python2.7 "
+    export PIP=$HOME/.miniconda/bin/pip
+    export INSTALL_PYTHON_DEPS_USING="conda"
 
 else
 
@@ -57,7 +74,7 @@ export OUR_TRAVIS_PATH=`pwd`
 
 export EIGEN_PATH=/usr/local/opt/eigen/include/eigen3
 export MATIO_PREFIX_PATH=/usr/local/opt/libmatio
-export MATIO_LIBRARY=$MATIO_PREFIX_PATH/lib/libmatio.a
+export MATIO_LIBRARY=$MATIO_PREFIX_PATH/lib/libmatio.dylib
 export CMAKE_PATH=/usr/local/opt/cmake
 export PYBIND11_PREFIX_PATH=/usr/local/opt/pybind11
 
@@ -106,6 +123,57 @@ if [[ "$INSTALL_PYTHON_DEPS_USING" == "brew-pip" ]]; then
 
     $PIP install $PIP_EXTRAS numpy scipy cvxpy >pip_output.txt 2>&1 || cat pip_output.txt
 
+elif [[ "$INSTALL_PYTHON_DEPS_USING" == "conda" ]]; then
+
+    wget "$MINICONDA_INSTALLER" -O miniconda.sh
+    
+    bash miniconda.sh -b -p "$HOME/.miniconda"
+    export PATH="$HOME/.miniconda/bin:$PATH"
+
+    hash -r
+
+    conda config --set always_yes yes --set changeps1 no
+
+    #conda config --add channels conda-forge
+    #conda config --add channels cvxgrp
+
+    conda update -q conda
+    # Useful for debugging any issues with conda
+    conda info -a
+
+    # MKL gives us problems ... ARRRRRGHHHH!!!!
+    #conda install nomkl
+
+    conda install libgcc libgfortran
+
+    conda install gcc
+
+    # ### why bother with environments?
+    # conda create -q -n test-environment python=3.6 pip ...
+
+    conda install numpy scipy matplotlib ecos
+    conda install -f numpy
+    conda install -c cvxgrp scs multiprocess cvxcanon cvxpy
+    conda install -c conda-forge pybind11
+
+    if [[ "$TT_CC" =~ ^clang.*$ ]]; then
+        #
+        # PATCH pybind11 for compilation with clang using libstdc++ on gcc-4.8
+        #
+        pybind11include=`$PYTHON_EXECUTABLE -c 'import pybind11; assert(pybind11.__version__ == "2.2.0"); print(pybind11.get_include());'`
+        (cd "$pybind11include" && patch -p2 <test/travis/fix_clang-libstdcxx-gcc4_for_pybind-2-2-0.patch)
+    fi
+
+    #source activate test-environment
+
+    python -c 'import pybind11; print("pybind11 version: {}".format(pybind11.__version__))'
+    python -c 'import numpy; print("NUMPY version: {}".format(numpy.__version__))'
+    python -c 'import scs; print("SCS version: {}".format(scs.__version__))'
+    python -c 'import cvxpy; print("CVXPY version: {}".format(cvxpy.__version__))'
+    python -c 'import matplotlib; print("MATPLOTLIB version: {}".format(matplotlib.__version__))'
+
+    ls -lh $HOME/.miniconda/lib/
+
 else
 
     echo >&2 "Error: INSTALL_PYTHON_DEPS_USING=$INSTALL_PYTHON_DEPS_USING  unknown !!!"
@@ -113,3 +181,10 @@ else
 
 fi
 
+
+
+if [[ "$INSTALL_BOOST_USING_CONDA_GCC" == 1 ]]; then
+
+    brew install boost --cc="$CMAKE_C_COMPILER"
+
+fi
