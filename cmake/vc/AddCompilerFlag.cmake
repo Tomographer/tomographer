e98fad3012debd264cd3b0438bad8a7516a11469
Philippe Faist <phfaist@gmail.com>
2018-03-16 11:37:12 -0700

    updated vc arch cmake detection scripts

--- cmake/vc/AddCompilerFlag.cmake
+++ cmake/vc/AddCompilerFlag.cmake
@@ -41,7 +41,7 @@ include("${_currentDir}/CheckMicCCompilerFlag.cmake")
 include("${_currentDir}/CheckMicCXXCompilerFlag.cmake")
 
 macro(AddCompilerFlag _flag)
-   string(REGEX REPLACE "[-.+/:= ]" "_" _flag_esc "${_flag}")
+   string(REGEX REPLACE "[-.+/:= ;]" "_" _flag_esc "${_flag}")
 
    set(_c_flags "CMAKE_C_FLAGS")
    set(_cxx_flags "CMAKE_CXX_FLAGS")
@@ -125,12 +125,37 @@ macro(AddCompilerFlag _flag)
       __m128 foo(__m128 x) { return _mm_fmadd_ps(x, x, x); }
       int main() { return 0; }")
       set(_cxx_code "${_c_code}")
+   elseif("${_flag}" STREQUAL "-std=c++14" OR "${_flag}" STREQUAL "-std=c++1y")
+      set(_cxx_code "#include <utility>
+      struct A { friend auto f(); };
+      template <int N> constexpr int var_temp = N;
+      template <std::size_t... I> void foo(std::index_sequence<I...>) {}
+      int main() { foo(std::make_index_sequence<4>()); return 0; }")
+   elseif("${_flag}" STREQUAL "-std=c++17" OR "${_flag}" STREQUAL "-std=c++1z")
+      set(_cxx_code "#include <functional>
+      int main() { return 0; }")
    elseif("${_flag}" STREQUAL "-stdlib=libc++")
       # Compiling with libc++ not only requires a compiler that understands it, but also
       # the libc++ headers itself
       set(_cxx_code "#include <iostream>
       #include <cstdio>
       int main() { return 0; }")
+   elseif("${_flag}" STREQUAL "-march=knl"
+         OR "${_flag}" STREQUAL "-march=skylake-avx512"
+         OR "${_flag}" STREQUAL "/arch:AVX512"
+         OR "${_flag}" STREQUAL "/arch:KNL"
+         OR "${_flag}" MATCHES "^-mavx512.")
+      # Make sure the intrinsics are there
+      set(_cxx_code "#include <immintrin.h>
+      __m512 foo(__m256 v) {
+        return _mm512_castpd_ps(_mm512_insertf64x4(_mm512_setzero_pd(), _mm256_castps_pd(v), 0x0));
+      }
+      __m512i bar() { return _mm512_setzero_si512(); }
+      int main() { return 0; }")
+   elseif("${_flag}" STREQUAL "-mno-sse" OR "${_flag}" STREQUAL "-mno-sse2")
+      set(_cxx_code "#include <cstdio>
+      #include <cstdlib>
+      int main() { return std::atof(\"0\"); }")
    else()
       set(_cxx_code "#include <cstdio>
       int main() { return 0; }")
